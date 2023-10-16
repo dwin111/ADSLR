42c2e11fdf45e0d41bafea4b4f1b56a78c449f98
Dmytro Dudchenko <mr.deymys.12@gmail.com>
2023-10-17 02:55:20 +0300

    Add mergesort

---
+++ SelectionSort/MergeSort.cs
@@ -0,0 +1,56 @@
+﻿
+namespace SelectionSort
+{
+    public static class MergeSort
+    {
+
+        public static double[] SortMergeArray(this double[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = left + (right - left) / 2;
+                SortMergeArray(array, left, middle);
+                SortMergeArray(array, middle + 1, right);
+                MergeArray(array, left, middle, right);
+            }
+            return array;
+        }
+
+        private static void MergeArray(double[] array, int left, int middle, int right)
+        {
+            var leftArrayLength = middle - left + 1;
+            var rightArrayLength = right - middle;
+            var leftTempArray = new double[leftArrayLength];
+            var rightTempArray = new double[rightArrayLength];
+            int i, j;
+            for (i = 0; i < leftArrayLength; ++i)
+                leftTempArray[i] = array[left + i];
+            for (j = 0; j < rightArrayLength; ++j)
+                rightTempArray[j] = array[middle + 1 + j];
+            i = 0;
+            j = 0;
+            int k = left;
+            while (i < leftArrayLength && j < rightArrayLength)
+            {
+                if (leftTempArray[i] <= rightTempArray[j])
+                {
+                    array[k++] = leftTempArray[i++];
+                }
+                else
+                {
+                    array[k++] = rightTempArray[j++];
+                }
+            }
+            while (i < leftArrayLength)
+            {
+                array[k++] = leftTempArray[i++];
+            }
+            while (j < rightArrayLength)
+            {
+                array[k++] = rightTempArray[j++];
+            }
+        }
+
+
+    }
+}
